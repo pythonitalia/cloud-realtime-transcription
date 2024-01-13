4843865c17a1ea6cd5d4ce4b9a140945b58c603f
Marco Acierno <marcoaciernoemail@gmail.com>
2024-01-13 18:41:12 +0100

    host

--- run-server-prod.sh
+++ run-server-prod.sh
@@ -1,4 +1,4 @@
 cd server
 
-DEVICE=cuda:0 ATTN_IMPLEMENTATION=flash_attention_2 ./.venv/bin/uvicorn src.main:app --reload --port 3535
+DEVICE=cuda:0 ATTN_IMPLEMENTATION=flash_attention_2 ./.venv/bin/uvicorn src.main:app --host 0.0.0.0 --reload --port 3535
 cd -
