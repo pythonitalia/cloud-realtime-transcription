6cfa6eccdd710452e8b4e54cc3a2548289625a59
Marco Acierno <marcoaciernoemail@gmail.com>
2024-01-19 02:46:23 +0100

    flash attn 1

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-./.venv/bin/ninja --version
-echo $?
-
-cd $HOME/code
-ls -al
-ls .venv/ -al
-ls .venv/bin/ -al
-ls .venv/lib/python3.11/site-packages/ -al
-
 curl -sS https://bootstrap.pypa.io/get-pip.py | ./.venv/bin/python
-./.venv/bin/python -m pip install flash_attn --no-build-isolation
+./.venv/bin/python -m pip install flash_attn==1.0.9 --no-build-isolation
 
 echo "Installation done"
 
